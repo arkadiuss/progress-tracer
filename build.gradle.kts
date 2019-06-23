a488e80fea1b39fa24ae7a45e8ef467429a5b3af
Arkadiusz Kraus <arkadius.kraus@gmail.com>
2019-06-23 15:43:17 +0200

    switch gradle files to kotlin dsl

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    kotlin("android.extensions")
+}
+
+android {
+    compileSdkVersion(28)
+
+    defaultConfig {
+        minSdkVersion(15)
+        targetSdkVersion(28)
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf(".jar"))))
+    implementation("com.android.support:appcompat-v7:28.0.0")
+    testImplementation("junit:junit:4.12")
+    androidTestImplementation("com.android.support.test:runner:1.0.2")
+    androidTestImplementation("com.android.support.test.espresso:espresso-core:3.0.2")
+}
+repositories {
+    mavenCentral()
+}
